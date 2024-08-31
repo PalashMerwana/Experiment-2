cfa7513e03d65f8b6907271e2ec6bc17c96d6448
PalashMerwana <palash.merwana.btech2023@sitpune.edu.in>
2024-08-31 10:03:36 +0530

    Add files via upload

---
+++ sizeof_2.cpp
@@ -0,0 +1,25 @@
+//Palash Merwana 
+//Prn: 23070123095
+//experiment 3: opertators 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter any integer: ";
+    cin>>a;
+    cout<<"\n integer = "<<a<<" and size is "<<sizeof(a)<<" bytes";
+    
+    float b;
+    cout<<"\n enter a number: ";
+    cin>>b;
+    cout<<"\n Float= "<<b<<" and size is "<<sizeof(b)<<" bytes";
+    
+    short int c;
+    cout<<"\nenter an interger: ";
+    cin>>c;
+    cout<<"\nShort integer = "<<c<<" and size is "<<sizeof(c)<<" bytes";
+    return 0;
+}
---
+++ sizeof_3.cpp
@@ -0,0 +1,25 @@
+//Palash Merwana 
+//Prn: 23070123095
+//experiment 3: opertators 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter any integer: ";
+    cin>>a;
+    cout<<"\n integer ="<<a<<" and size is "<<sizeof(a)<<" bytes";
+    
+    int register b;
+    cout<<"\n enter a number: ";
+    cin>>b;
+    cout<<"\n REGISTER= "<<b<<" and size is "<<sizeof(b)<<" bytes";
+    
+    int auto c;
+    cout<<"\n enter an interger: ";
+    cin>>c;
+    cout<<"\n Auto = "<<c<<" and size is "<<sizeof(c)<<" bytes";
+    return 0;
+}
